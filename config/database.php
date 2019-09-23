5457be4e5a8cd073a5c24be64caf999f1bb0b6ee
Thiên Hồ <thienht1997tt@gmail.com>
2019-09-23 23:27:18 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('mysql://bd5f49114208a2:0eef053f@us-cdbr-iron-east-02.cleardb.net/heroku_c4b7cc173005165?reconnect=true'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_c4b7cc173005165'),
+            'username' => env('DB_USERNAME', 'bd5f49114208a2'),
+            'password' => env('DB_PASSWORD', '0eef053f'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
